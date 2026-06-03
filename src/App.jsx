7ff5a059899941084831e193169d301ead110d2b
Bryne <228299496+b1yn3@users.noreply.github.com>
2026-06-03 19:51:56 +0800

    feat: temporary structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,63 @@
 import './App.css'
+import './styles/grid.css'
+import image from './assets/image.png' 
 
 function App() {
   return (
     <>
-    <p>HELLO WORLD</p>
+    <Header />
+    <Main />
     </>
+  )
+}
+
+function Header(){
+  return (
+    <header>
+      <h3>Current Score: </h3>
+      <h3>High Score: </h3>
+    </header>
+  )
+}
+
+function Main(){
+  return (
+    <main>
+      <div className="card-container">
+        <div className="card">
+          <img src={image}></img>
+          <p>Sample Name</p>
+        </div>
+
+        <div className="card">
+          <img src={image}></img>
+          <p>Sample Name</p>
+        </div>
+
+        <div className="card">
+          <img src={image}></img>
+          <p>Sample Name</p>
+        </div>
+
+        <div className="card">
+          <img src={image}></img>
+          <p>Sample Name</p>
+        </div>
+
+        <div className="card">
+          <img src={image}></img>
+          <p>Sample Name</p>
+        </div>
+
+        <div className="card">
+          <img src={image}></img>
+          <p>Sample Name</p>
+        </div>
+      </div>
+
+      
+
+    </main>
   )
 }
 
